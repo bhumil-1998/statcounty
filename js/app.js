c62c509bcd2dc0bcd761e24323446412cbd43208
bhumil-1998 <bhumilvyas007@gmail.com>
2022-05-02 17:25:00 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+
+/*********************************/
+/*         INDEX                 */
+/*================================
+ *     01.  Menu sticky          *
+ *     02.  Toggle Menus         *
+ *     03.  Active Menu          *
+ *     04.  Clickable Menu       *
+ *     05.  Back to top          *
+ *     06.  Feather icon         *
+ *     06.  DD Menu              *
+ *     06.  Active Sidebar Menu  *
+ *     07.  Contact us           *
+ ================================*/
+
+ 
+ // Menu sticky js
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if(navbar!=null){
+        if (
+            document.body.scrollTop >= 50 ||
+            document.documentElement.scrollTop >= 50
+        ) {
+            navbar.classList.add("nav-sticky");
+        } else {
+            navbar.classList.remove("nav-sticky");
+        }
+    }
+}
+
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+})
